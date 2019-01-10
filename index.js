7869f9611a87337b6db66a6f47a5118bca8b4fdf
Mai Tung <goingdownfr@gmail.com>
2019-01-10 23:18:25 +0700

    das

--- index.js
+++ index.js
@@ -1,22 +1,23 @@
 let express = require('express');
 let request = require('request');
+let puppeteer = require('puppeteer');
 
 let app = express();
-let urlJP = "https://asia-northeast1-alibaba-baidu.cloudfunctions.net/screenshot?" +
-    "url=https://fir-maps-e4e81.firebaseapp.com/";
-let urlCN = "https://asia-northeast1-alibaba-baidu.cloudfunctions.net/screenshot?" +
-    "url=https://fir-maps-e4e81.firebaseapp.com/osm/";
+let urlJP = "https://fir-maps-e4e81.firebaseapp.com/";
+// let urlJCN = "https://fir-maps-e4e81.firebaseapp.com/osm/";
 
 // respond with "hello world" when a GET request is made to the homepage
-app.get('/', function (req, res) {
-    request.get({
-        url : urlJP,
-        time : true
-    },function(err, response, body){
-        console.log('Request time in ms', response.elapsedTime);
-        console.log('Body', body);
+app.get('/', async function (req, res) {
+    const browser = await puppeteer.launch({
+        headless: true,
+        args: ['--no-sandbox']
     });
-    res.send('hello')
+    const page = await browser.newPage();
+    await page.goto(urlJP);
+    const imageBuffer = await page.screenshot();
+    res.set('Content-Type', 'image/png');
+    res.send(imageBuffer);
+
 });
 
 app.set('port', process.env.PORT || 3000);
