9aca568d296a633902aa4c078d41d7f3ce4a7f61
maitungmn <goingdownfr@gmail.com>
2019-01-16 21:07:14 +0700

    added live-demo branch

--- index.js
+++ index.js
@@ -11,17 +11,20 @@ app.use(function(req, res, next) {
     next();
 });
 
-// let urlJP = "https://fir-maps-e4e81.firebaseapp.com/";
-let urlJCN = "https://www.openstreetmap.org/#map=13/35.6919/139.7210";
+let urlJP = "https://fir-maps-e4e81.firebaseapp.com/";
+// let urlCN = "https://www.openstreetmap.org/#map=13/35.6919/139.7210";
 
 // respond with "hello world" when a GET request is made to the homepage
 app.get('/', async function (req, res) {
     res.set('Access-Control-Allow-Origin', "*");
     res.set('Access-Control-Allow-Methods', 'GET, POST');
-    const browser = await puppeteer.launch({args: ['--no-sandbox']});
+    const browser = await puppeteer.launch({
+        headless: false,
+        args: ['--no-sandbox']
+    });
     const page = await browser.newPage();
     let start = new Date();
-    await page.goto(urlJCN);
+    await page.goto(urlJP);
     const imageBuffer = await page.screenshot();
     res.setHeader('Content-Type', 'application/json');
     res.send(JSON.stringify({time: new Date() - start, image: imageBuffer}));
