16b10fa1203aca21d491a40dc28523cf60647bcd
Mai Tung <goingdownfr@gmail.com>
2019-01-19 20:28:45 +0700

    revert

--- index.js
+++ index.js
@@ -17,11 +17,11 @@ app.get('/', async function (req, res) {
     await page.setViewport(override);
     await page.goto(urlCN);
     const imageBuffer = await page.screenshot();
-    // res.setHeader('Content-Type', 'application/json');
-    // res.send(JSON.stringify({time: new Date() - start, image: imageBuffer}));
+    res.setHeader('Content-Type', 'application/json');
+    res.send(JSON.stringify({time: new Date() - start, image: imageBuffer}));
 
-    res.set('Content-Type', 'image/png');
-    res.send(imageBuffer);
+    // res.set('Content-Type', 'image/png');
+    // res.send(imageBuffer);
 
 });
 
@@ -33,13 +33,21 @@ app.get('/jp', async function (req, res) {
     const page = await browser.newPage();
     const override = Object.assign(page.viewport(), {width: 1000});
     await page.setViewport(override);
-    await page.goto(urlJP);
-    const imageBuffer = await page.screenshot();
-    // res.setHeader('Content-Type', 'application/json');
-    // res.send(JSON.stringify({time: new Date() - start, image: imageBuffer}));
+    try {
+        await page.goto(urlJP);
+        const imageBuffer = await page.screenshot();
+        res.setHeader('Content-Type', 'application/json');
+        res.send(JSON.stringify({time: new Date() - start, image: imageBuffer}));
+    } catch (e) {
+        res.setHeader('Content-Type', 'application/json');
+        res.send(JSON.stringify({time: new Date() - start, image: e}));
+    } finally {
+        await page.close()
+    }
 
-    res.set('Content-Type', 'image/png');
-    res.send(imageBuffer);
+
+    // res.set('Content-Type', 'image/png');
+    // res.send(imageBuffer);
 
 });
 
